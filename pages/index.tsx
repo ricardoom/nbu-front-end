3ac92df85a08018bbe71c7a357dd4ec45403588a
ricardoom <ricardo.e.galvez@gmail.com>
2022-05-22 09:57:53 -0700

    feat(global): :construction: Still work in progress, but the preview issue is sorted out
    Needed to bring in the Portable Text component to get the body copy to render correctly in previews and in the regular pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,16 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import { createClient } from 'next-sanity';
 import { PortableText } from '@portabletext/react';
-
+import { config } from '../lib/config';
 
 const IndexPage: NextPage = ({ data }: any) => {
   return (
     <>
       <header>
         <h1>{data.homepage.title}</h1>
+        <h2>{data.homepage.subtitle}</h2>
       </header>
       <main>
-        <h2>{data.homepage.subtitle}</h2>
         <PortableText
           value={data.homepage.body}
         />
@@ -24,27 +24,23 @@ const IndexPage: NextPage = ({ data }: any) => {
 
 export default IndexPage;
 
-const client = createClient({
-  projectId: 'vfd9jugo', // This stuff should be abstracted no?
-  dataset: 'production',
-  apiVersion: '2022-03-25',
-  useCdn: false,
-});
+
+const client = createClient(config);
 
 export async function getStaticProps() {
   const pets: any[] = await client.fetch(`*[_type == 'pet']`);
-  const homepage = await client.fetch(`*[_type == "homepage"][0] {
+  const homepage: any[] = await client.fetch(`*[_type == "homepage"][0] {
     title,
     subtitle,
     body,
   }`
   );
 
-  const data = { homepage, pets };
+  const data = { homepage };
   return {
     props: {
       data
     },
-    revalidate: 1,
+    revalidate: 1, // TODO: look into what this line is doing
   };
 }
