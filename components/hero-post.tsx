d418eddc3c05cd309cab2d31c15f48317527dc5e
ricardoom <ricardo.e.galvez@gmail.com>
2022-05-29 08:12:01 -0700

    feat(Blog): :construction: Works in progress to get this going in the proper way. also pushing things up gh

---
+++ components/hero-post.tsx
@@ -0,0 +1,41 @@
+import Link from 'next/link'
+import Date from './date'
+
+
+interface Props {
+  title: string | any,
+  // coverImage: string | any,
+  // date: string | any,
+  excerpt: string | any,
+  author: string | any,
+  slug: string,
+}
+
+export default function HeroPost({
+  title,
+  // coverImage,
+  // date,
+  excerpt,
+  author,
+  slug,
+}: Props) {
+  return (
+    <section>
+      <div className="">
+        <div>
+          <h3 className="">
+            <Link href={`/posts/${slug}`}>
+              <a className="">{title}</a>
+            </Link>
+          </h3>
+          <div className="dateString">
+            {/* <Date dateString={date} /> */}
+          </div>
+        </div>
+        <div>
+          <p className="">{excerpt}</p>
+        </div>
+      </div>
+    </section>
+  )
+}
