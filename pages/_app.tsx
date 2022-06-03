34b93b3b9a74c9bbd00815ae8c5ffa9b14a1caca
ricardoom <ricardo.e.galvez@gmail.com>
2022-06-03 13:12:36 -0700

    feat(Design Theme UI): :lipstick: Added Open Props as the theme driver and design framework / guide star

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import 'open-props/style';
+import 'open-props/normalize';
+import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
