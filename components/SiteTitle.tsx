f5b03c2501adf340b87cc8a911a7abf72307e636
ricardoom <ricardo.e.galvez@gmail.com>
2022-06-05 13:53:54 -0700

    feat(global): :globe_with_meridians: Improved styles for type - added a new component - more customiztion of Open Props

--- components/SiteTitle.tsx
+++ components/SiteTitle.tsx
@@ -1,14 +1,31 @@
-import Link from "next/link"
+import { useRouter } from 'next/router'
+import Link from "next/link";
+import titles from '../styles/component/Title.module.scss';
+
+
+
 
 const SiteTitle = () => {
+  const { asPath } = useRouter();
+  console.log(asPath);
   return (
-    <header>
-      <h1>
-      <Link href='/'>
-      Native Bound Unbound
+    <header className="center">
+      {asPath !== '/' ? 
+      <h1 className={titles.headLine}>
+        <Link href='/'>
+      <a>
+      <span>Native Bound</span>&nbsp;<span className={titles.unbound}>Unbound</span>
+      </a>
       </Link>
       </h1>
-      <h2>Archive of Native American Enslavement</h2>
+      : // TODO: Refactor this big time! 
+      <h1 className={titles.headLine}>
+      
+      <span>Native Bound</span>&nbsp;<span className={titles.unbound}>Unbound</span>
+      
+      
+    </h1>}
+      <h2 className={titles.subHeadline}>Archive of Native American Enslavement</h2>
     </header>
   )
 }
